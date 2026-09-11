defd553e8c709456339054da00d1952139ffcca4
takwanashava-spec <takwana.shava@gmail.com>
2026-09-11 10:10:24 +0200

    Show seconds on the game clock

--- components/game/game-shell.tsx
+++ components/game/game-shell.tsx
@@ -67,6 +67,7 @@ export function GameShell({
         {
           hour: "2-digit",
           minute: "2-digit",
+          second: "2-digit",
           hour12: false,
           timeZone: "UTC",
         },
